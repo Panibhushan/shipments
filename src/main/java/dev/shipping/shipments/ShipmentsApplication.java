87e9e630d09b1ec378fcb71cfa0c1324a0d923a1
Panibhushan <56889823+Panibhushan@users.noreply.github.com>
2026-05-25 11:58:43 +0530

    Update ShipmentsApplication.java -- to ignore and prevent app from crashing when the .env file is not found

--- src/main/java/dev/shipping/shipments/ShipmentsApplication.java
+++ src/main/java/dev/shipping/shipments/ShipmentsApplication.java
@@ -7,25 +7,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShipmentsApplication {
     public static void main(String[] args) {
-      
-    	System.out.println("Working directory: " + System.getProperty("user.dir"));
-    	
-    	Dotenv dotenv = Dotenv.configure()
-    		    .directory("./")
-    		    .load();
-    	 
-    	dotenv.entries().forEach(entry -> 
-    	    System.setProperty(entry.getKey(), entry.getValue())
-    	);
 
-		/*
+        System.out.println("Working directory: " + System.getProperty("user.dir"));
+
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()  // ← Will fetch data from .env when running in local, but when deployed in EC2, if the .env is not found then it won't crash if .env not found on EC2
+                .load();
+
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+        
+        /*
 		 * // Debug - print to confirm values loaded System.out.println("DB URL: " +
 		 * System.getProperty("DB_URL_LOCAL")); System.out.println("DB USER: " +
 		 * System.getProperty("DB_USERNAME_LOCAL")); System.out.println("DB PASSWORD: "
 		 * + System.getProperty("DB_PASSWORD_LOCAL"));
 		 */
 
-
-    	SpringApplication.run(ShipmentsApplication.class, args);
+        SpringApplication.run(ShipmentsApplication.class, args);
     }
 }
